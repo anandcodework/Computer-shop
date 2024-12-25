64ad060731f2e485f38e6f63703b3a46e9d0d748
anandcodework <am6429975@gmail.com>
2024-12-25 15:48:37 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ if (!fs.existsSync(invoicesDir)) {
 
 // CORS Configuration
 app.use(cors({
-    origin: http://localhost:3000,
+    origin: process.env.FRONTEND_URL,
     credentials: true
 }));
 
